dc989a17cbe72ab7296eb10e5e858accd689fdc4
email <ayyaznaqvi@gmail.com>
2025-03-24 19:51:47 +0100

    Initial commit: Added Logical operators.py

---
+++ Logical operators.py
@@ -0,0 +1,23 @@
+#logical
+
+x = 10
+y = 20
+
+#and operator
+if x > y and x < y:
+    print("your both conditions are true")
+else:
+    print("try again")
+
+#or operator
+if x > y or x < y:
+    print("your both conditions are not true")
+else:
+    print("try again")
+
+#not operator
+if(not(x>y) and (x<y)):
+    print("your both conditions are true")
+else:
+    print("try again")
+
