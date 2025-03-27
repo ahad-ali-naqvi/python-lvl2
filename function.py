5095790540054c4093b0e5b295b5601602edd3ef
email <ayyaznaqvi@gmail.com>
2025-03-27 19:27:31 +0100

    initial commit

---
+++ function.py
@@ -0,0 +1,21 @@
+#defining  a function
+def ramadan_special():
+    print("fasting")
+    print("ramadan mubarak")
+    print("iftaar")
+    print("sehri")
+    print("praying")
+    print("taraweeh")
+    print("quran")
+    print("laylatul qadr")
+    print("odd nights")
+    print("eid")
+ramadan_special()
+
+
+def eid_special():
+     print("gifts")
+     print("eid  mubarak")
+     print("fun")
+     print("eid money")
+eid_special()
